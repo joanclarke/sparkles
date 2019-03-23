1315cd72d290a2ff51842f993a0ca25491a29b14
Joan Clarke <joanclarke100@gmail.com>
2019-03-23 16:46:49 -0500

    add toggleImgs() function

--- js/sparkles.js
+++ js/sparkles.js
@@ -11,3 +11,16 @@ $(function () {
 
 
 
+function toggleImgs() {
+  var hideShowImages = document.getElementsByClassName("hide-show-images");
+  for(var i = 0; i < hideShowImages.length; i++){
+    if (hideShowImages[i].style.display === "none") {
+      hideShowImages[i].style.display = "block";
+    } else {
+      hideShowImages[i].style.display = "none";
+    }
+  }
+}
+
+
+
