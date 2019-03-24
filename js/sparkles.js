2be20a6a617b2a75bf3ec46a507deb1b9480b8a4
Joan Clarke <joanclarke100@gmail.com>
2019-03-23 19:28:40 -0500

    add contact page and heading background

--- js/sparkles.js
+++ js/sparkles.js
@@ -11,16 +11,53 @@ $(function () {
 
 
 
-function toggleImgs() {
-  var hideShowImages = document.getElementsByClassName("hide-show-images");
-  for(var i = 0; i < hideShowImages.length; i++){
-    if (hideShowImages[i].style.display === "none") {
-      hideShowImages[i].style.display = "block";
-    } else {
-      hideShowImages[i].style.display = "none";
+// function toggleImgs() {
+//   var hideShowImages = document.getElementsByClassName("hide-show-images");
+//   changeBtnText(); // Invoking changeBtnText added
+//   for(var i = 0; i < hideShowImages.length; i++){
+//     if (hideShowImages[i].style.display === "none") {
+//       hideShowImages[i].style.display = "block";
+//       changeBtnText();
+//     } else {
+//       hideShowImages[i].style.display = "none";
+//     }
+//   }
+// }
+
+// function changeBtnText() {
+//   var hideShowImgBtn = document.getElementsByClassName("hide-show-img-btn");
+//   for(var i = 0; i < hideShowImgBtn.length; i++)
+//     if(hideShowImgBtn[i].innerText =="More Images") {
+//       hideShowImgBtn[i].innerText = "Less Images";
+//     }else {
+//       hideShowImgBtn[i].innerText = "More Images";
+//     }
+//   }
+
+
+  function toggleImgs() {
+    var hideShowImages = document.getElementsByClassName("hide-show-images");
+    changeBtnText(); // Invoking changeBtnText added
+    for(var i = 0; i < hideShowImages.length; i++){
+      if (hideShowImages[i].style.display === "none") {
+        hideShowImages[i].style.display = "block";
+        changeBtnText();
+      } else {
+        hideShowImages[i].style.display = "none";
+      }
     }
   }
-}
+  
+  function changeBtnText() {
+    var hideShowImgBtn = document.getElementsByClassName("hide-show-img-btn");
+    for(var i = 0; i < hideShowImgBtn.length; i++)
+      // Using innerText instead of value
+      if(hideShowImgBtn[i].innerText =="More Images") {
+        hideShowImgBtn[i].innerText = "Less Images";
+      }else {
+        hideShowImgBtn[i].innerText = "More Images";
+      }
+    }
 
 
 
